632ed650bdde1264187b359a53ec039ba4d09cfe
JuniosSTL <junios@naver.com>
2017-09-02 16:10:24 +0900

    Call by Refereence 함수 오버로딩(Function Overloading)

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -10,13 +10,63 @@ namespace Day03
     {
         static void Main(string[] args)
         {
-            int number = Add3(5);
+            int a = 3;
+            int b = 5;
+
+            float c = 3.5f;
+            float d = 5.3f;
+
+            Swap(a, b);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Swap(ref c, ref d);
+
+            Console.WriteLine(c);
+            Console.WriteLine(d);
         }
 
-        static int Add3(int x)
+        /// <summary>
+        /// call by Value
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        static void Swap(int a, int b)
         {
-           return (x + 3);
+            int c = 0;
+
+            c = a;
+            a = b;
+            b = c;
         }
 
+        /// <summary>
+        /// Call by reference
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        static void Swap(ref int a, ref int b)
+        {
+            int c = 0;
+
+            c = a; //main->a
+            a = b; //main->b
+            b = c;
+        }
+
+        /// <summary>
+        /// Call by Reference float Type
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        static void Swap(ref float a, ref float b)
+        {
+            float c = 0;
+
+            c = a; //main->a
+            a = b; //main->b
+            b = c;
+        }
     }
 }
